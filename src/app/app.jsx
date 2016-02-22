100cd333df521b174ce1bcaa2759870ed6951168
ee7klt <kltah1@gmail.com>
2016-02-22 19:48:02 +0800

    separate concerns for individual counter and the set of counters

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -3,17 +3,32 @@ import deepFreeze from 'deep-freeze';
 
 
 
+const counter =(state = {}, action ) => {
+// state is now a single counter
+  switch(action.type) {
+    case 'INCREMENT':
+      if (state.id === action.id)
+        return {...state, count: state.count+1};
+      else return state;
+
+      case 'DECREMENT':
+        if (state.id === action.id)
+          return {...state, count: state.count-1};
+        else return state;
+
+
+    default: state
+  }
+}
 
 const counters = (state ={},action) => {
 
 switch (action.type) {
+  // c is a single counter.
 case 'INCREMENT':
- return state.map(counter => {
-   if (counter.id === action.id) {
-     return {...counter, count: counter.count+1};
-     }
-   else return counter;
- }).sort(function(a,b) {return a.count-b.count}).reverse();
+case 'DECREMENT':
+ return state.map(c => counter(c, action))
+    .sort(function(a,b) {return a.count-b.count}).reverse();
 
 default:
 return state;
@@ -35,4 +50,19 @@ console.log('testIncrement passed');
 
 }
 
+
+const testDecrement = () => {
+
+const stateBefore =  [{id:0, count:5},{id:1, count:5},{id:2, count:1}];
+const stateAfter =  [{id:1, count:5},{id:0, count:4},{id:2, count:1}];
+const action = {type: 'DECREMENT', id: 0};
+deepFreeze(stateBefore);
+deepFreeze(action);
+expect (counters(stateBefore,action)).toEqual(stateAfter)
+console.log('testDecrement passed');
+
+}
+
+
 testIncrement();
+testDecrement();
