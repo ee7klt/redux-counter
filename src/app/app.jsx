d80b9173fb31cadca469261dbf0b3510e1338c25
ee7klt <kltah1@gmail.com>
2016-02-22 22:00:56 +0800

    addCount reducer keeps track of number of counters added. it is triggered by ADD_COUNTER action type

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -5,8 +5,16 @@ import deepFreeze from 'deep-freeze';
 
 
 
+// reducer composition concept 1: different reducers specify how
+// differnt parts of the state tree are updated in response
+// to the actions.
+// counter is called from within counters.
+
+
 const counter =(state = {}, action ) => {
-// state is now a single counter
+// separate councerns between an individual counter
+// and the counters overall.
+// state is now a single counter here
   switch(action.type) {
     case 'INCREMENT':
       if (state.id === action.id)
@@ -18,7 +26,8 @@ const counter =(state = {}, action ) => {
           return {...state, count: state.count-1};
         else return state;
 
-
+case 'ADD_COUNTER':
+   return  {id:action.id, count: 0 }
     default: state
   }
 }
@@ -34,7 +43,7 @@ case 'DECREMENT':
     .sort(function(a,b) {return a.count-b.count}).reverse();
 
 case 'ADD_COUNTER':
-  return [...state, {id: state.length, count: 0 }];
+  return [...state, counter(state,action)];
 
 
 default:
@@ -43,20 +52,37 @@ return state;
 };
 };
 
+// this is an example of how we can use a
+// common action type to trigger changes to state/
+// in two separate reducers.
+// In this case ADD_COUNTER adds a new counter in counters
+// as well increments the count of counters added.
+const addCount = (state = 0, action) => {
+  switch (action.type) {
+  case 'ADD_COUNTER':
+    return state+1;
+  default:
+    return state;
+  }
+}
 
-
+// reducer composition concept 2: combine reducers
+// state is not combined state output from all the reducers
+// wrap counters in to a master state
+// as a key value pair with the counters in a value of key 'counters'
 const counterApp = (state = {}, action) => {
   return {
     counters: counters(state.counters, action),
+    addCount: addCount(state.addCount, action),
   }
 
 }
 
 const testAddCounter = () => {
 
-  const stateBefore = {counters: [{id:0, count:5},{id:1, count:5},{id:2, count:1}]};
-  const stateAfter =  {counters:[{id:0, count:5},{id:1, count:5},{id:2, count:1}, {id: 3, count: 0} ]};
-  const action = {type: 'ADD_COUNTER'};
+  const stateBefore = {counters: [{id:0, count:5},{id:1, count:5},{id:2, count:1}], addCount: 3};
+  const stateAfter =  {counters:[{id:0, count:5},{id:1, count:5},{id:2, count:1}, {id: 3, count: 0} ], addCount: 4};
+  const action = {type: 'ADD_COUNTER', id: 3};
   deepFreeze(stateBefore);
   deepFreeze(action);
   expect (counterApp(stateBefore,action)).toEqual(stateAfter)
@@ -89,6 +115,6 @@ console.log('testDecrement passed');
 }
 
 
-testIncrement();
-testDecrement();
+//testIncrement();
+//testDecrement();
 testAddCounter();
