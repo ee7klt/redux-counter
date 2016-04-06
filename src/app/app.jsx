36811bc998c7eccf731f034bb064664b5d0a8733
ee7klt <kltah1@gmail.com>
2016-04-06 22:27:03 +0800

    trying to map over the counters

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,22 +1,62 @@
 import expect, { createSpy, spyOn, isSpy } from 'expect';
 import deepFreeze from 'deep-freeze';
 import {createStore} from 'redux';
-
-
 import {counterApp} from './counterApp.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Component} from 'react';
 
 
 
+const store = createStore(counterApp);
 
-const Buttons = () => (
-  <div>
+const Button = ({
+  count,
+}) => (
+  <span>
     <button>+</button>
-    <span>0</span>
+    <span>{count}</span>
     <button>-</button>
-  </div>
+  </span>
 
 )
 
-ReactDOM.render(<Buttons />,document.getElementById('app'));
+
+
+
+let nextCounterId = 0;
+const AddCounter = () => (
+    <button onClick = {() => {store.dispatch({
+    type: 'ADD_COUNTER', id:nextCounterId++,
+  })
+}}>Add Counter</button>
+)
+
+
+class Counters extends Component {
+  const state = store.getState();
+  const counters = state.counters;
+  render() {
+    return <div>
+      {
+        counters.map(
+          x => {
+            <Button>{x.count}</Button>
+
+    }
+  )
+  }
+  </div>
+
+  }
+}
+
+//Counters();
+const DisplayCounters = () => (
+  <div>
+  <Counters />
+  <AddCounter />
+  </div>
+)
+
+ReactDOM.render(<DisplayCounters />,document.getElementById('app'));
--- src/app/counterApp.jsx
+++ src/app/counterApp.jsx
@@ -12,7 +12,10 @@ import {addCount} from './addCount.jsx';
 // state is not combined state output from all the reducers
 // wrap counters in to a master state
 // as a key value pair with the counters in a value of key 'counters'
-export const counterApp = (state = {}, action) => {
+
+// counters : Array of counters
+// addCount : Number of counters added
+export const counterApp = (state = {counters:[], addCount: 0}, action) => {
   return {
     counters: counters(state.counters, action),
     addCount: addCount(state.addCount, action),
