69b026b8758cfdf2f17181e8a861bb6d3552af1a
ee7klt <kltah1@gmail.com>
2016-04-09 23:56:03 +0800

    Skin UI with Material-UI : AddCounterButton

--- src/app/AddCounterButton.jsx
+++ src/app/AddCounterButton.jsx
@@ -1,10 +1,11 @@
 
 import React from 'react';
+import RaisedButton from 'material-ui/lib/raised-button';
 
 const AddCounterButton = ({
   onButtonClick,
 }) => (
-  <button onClick = {onButtonClick}>Add Counter</button>
+  <RaisedButton onClick = {onButtonClick} label = 'Add Counter'/>
 );
 
 
--- src/app/Button.jsx
+++ src/app/Button.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import Paper from 'material-ui/lib/paper';
 
+const style = {
+  height: 100,
+  width: 100,
+  margin: 20,
+  textAlign: 'center',
+  display: 'inline-block',
+};
 
 const Button = ({
   count,
