dce7c34b132b4962bfddc5726f34a8e142c5c811
ee7klt <kltah1@gmail.com>
2016-04-12 00:39:53 +0800

    Add icons and flexbox positioning * todo: card font styling

--- src/app/Button.jsx
+++ src/app/Button.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
-import Paper from 'material-ui/lib/paper';
+import Card from 'material-ui/lib/card/card';
+import CardActions from 'material-ui/lib/card/card-actions';
+import CardHeader from 'material-ui/lib/card/card-header';
+import CardText from 'material-ui/lib/card/card-text';
+import FlatButton from 'material-ui/lib/flat-button';
+import SvgIcon from 'material-ui/lib/svg-icon';
+const buttonStyle ={
+  flexGrow: '2',
+}
 
-const style = {
-  height: 100,
-  width: 100,
-  margin: 20,
-  textAlign: 'center',
-  display: 'inline-block',
-};
+const liStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  listStyleType: 'none',
+  justifyContent: 'space-around',
+  margin: '10px',
+}
+
+const buttonContainerStyle = {
+    display: 'flex',
+  flexDirection: 'column',
+  flexGrow: '2',
+    justifyContent: 'space-around',
+}
+
+const cardStyle = {
+  flexGrow: '8',
+  fontSize: 'large',
+}
 
 const Button = ({
   count,
@@ -16,16 +36,33 @@ const Button = ({
   onDecrementClick,
   onRemoveClick,
 }) => (
-  <li  style = {{listStyleType: 'none'}}>
-    {id} <button onClick = {
+  <li  style = {liStyle}>
+    <div
+      style = {buttonContainerStyle}>
+      <FlatButton
+
+        onClick = {
           onIncrementClick
         }
-       >+</button>
-    <span>{count}</span>
-    <button onClick = {
-        onDecrementClick
-      }>-</button>
-    <button onClick = {onRemoveClick}>x</button>
+        icon={<i className="material-icons">keyboard_arrow_up</i>}
+        />
+
+      <FlatButton
+
+        onClick = {
+          onDecrementClick
+        }
+        icon = {<i className="material-icons">keyboard_arrow_down</i>}
+        />
+    </div>
+
+    <Card style={cardStyle}>
+      <CardText>{id}: {count} </CardText>
+    </Card>
+    <FlatButton
+      style={buttonStyle}
+      onClick = {onRemoveClick}
+      icon = {<i className="material-icons">clear</i>}/>
   </li>
 
 );
